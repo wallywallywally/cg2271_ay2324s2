e521af5700d3e3fd179494e5abd3efd09f35eec1
wallywallywally <willsonhanzhekai@gmail.com>
2024-03-12 12:53:23 +0800

    Add basic motor code

--- main.c
+++ main.c
@@ -1,4 +1,27 @@
 #include "MKL25Z4.h"
 
+/* LIBRARIES */
+#include "motor.h"
+
+
+/* MAIN */
+int main(void) {
+	SystemCoreClockUpdate();
+	
+	initMotor();
+	
+	// Test digital GPIO out
+//	PTD->PSOR = MASK(TEST_MOTOR);
+
+	// PWM for TPM0
+	// Forwards
+	TPM0_C0V = 3750;   // Left: A2, B2
+	TPM0_C2V = 1875;   // Right: A1, B1
+	
+	// Backwards
+//	TPM0_C1V = 6000;   // Left: A1, B1
+//	TPM0_C3V = 3000;   // Right: A2, B2
+	
+}
 
 
---
+++ motor.h
@@ -0,0 +1,8 @@
+#define MASK(x) (1 << (x))
+
+#ifndef MOTOR_CONTROL
+#define MOTOR_CONTROL
+
+void initMotor(void);
+
+#endif
